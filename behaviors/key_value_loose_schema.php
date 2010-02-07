b02cf019b8b23136b4b27ebf3a58801995e3d7b7
Takayuki Miwa <i@tkyk.name>
2010-02-07 15:27:37 +0900

    add License comment

--- behaviors/key_value_loose_schema.php
+++ behaviors/key_value_loose_schema.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * KeyValueLooseSchemaBehavior
+ * 
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ * Copyright (c) 2010 Takayuki Miwa <i@tkyk.name>
+ */
 
 class KeyValueLooseSchemaBehavior extends ModelBehavior
 {
